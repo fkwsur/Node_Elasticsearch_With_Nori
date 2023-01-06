d5031be2e76053d98a723e6db12053d9f1e50b0a
Hyun-Ji <guswl543210@gmail.com>
2023-01-06 14:00:03 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const app = require("express")()
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({
+  // cloud: { id: '<cloud-id>' },
+  // auth: { apiKey: 'base64EncodedKey' }
+  node : "http://localhost:9200"
+})
+
+async function run () {
+  // Let's start by indexing some data
+  await client.index({
+    index: 'game-of-thrones',
+    body: {
+      character: 'Ned Stark',
+      quote: 'Winter is coming.'
+    }
+  })
+
+  await client.index({
+    index: 'game-of-thrones',
+    body: {
+      character: 'Daenerys Targaryen',
+      quote: 'I am the blood of the dragon.'
+    }
+  })
+
+  await client.index({
+    index: 'game-of-thrones',
+    body: {
+      character: 'Tyrion Lannister',
+      quote: 'A mind needs books like a sword needs a whetstone.'
+    }
+  })
+
+  // here we are forcing an index refresh, otherwise we will not
+  // get any result in the consequent search
+  await client.indices.refresh({ index: 'game-of-thrones' })
+
+  // Let's search!
+  const result= await client.search({
+    index: 'game-of-thrones',
+    query: {
+      match: { quote: 'needs whetstone' }
+    }
+  })
+
+  console.log(result.hits.hits)
+}
+
+run().catch(console.log)
+
+app.listen(8084, () => {
+    console.log("hi")
+})
